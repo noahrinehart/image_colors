e79a848341e2720179fc424579f67cb319aae7ba
Noah Rinehart <rinehart.noah@gmail.com>
2017-08-25 10:27:37 -0400

    fixed module issues, implemented num_colors

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,44 @@
-use std::env;
+
 use std::path::Path;
 use std::collections::HashMap;
 use std::iter::FromIterator;
 
 extern crate image;
 
-mod lib {
-    fn fetchColors(filename: String, numColors: u32) {
-        let args: Vec<String> = env::args().collect();
-        let filename = &args[1];
+pub fn fetch_colors(filename: &String, num_colors: usize) {
 
-        let img = image::open(&Path::new(filename)).unwrap();
-        let raw_pixels = img.raw_pixels();
-        let raw_pixels_size = raw_pixels.len();
-        let mut pixel_map = HashMap::new();
+    let img = image::open(&Path::new(filename)).unwrap();
+    let raw_pixels = img.raw_pixels();
+    let raw_pixels_size = raw_pixels.len();
+    let mut pixel_map = HashMap::new();
 
-        let mut i = 0;
-        while i < raw_pixels_size {
-            let pixel = convert_nibbles_to_u32(&[raw_pixels[i], raw_pixels[i+1], raw_pixels[i+2]]);
-            *pixel_map.entry(pixel).or_insert(0) += 1;
-            i += 3;
-        }
-
-        let mut sorted_pixels = Vec::from_iter(pixel_map);
-        sorted_pixels.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
-
-        for i in 0..5 {
-            match sorted_pixels.get(i) {
-                Some(&(pixel, count)) => {
-                    println!("#{:X} - {} pixels", pixel, count);
-                },
-                None => break,
-            }
-        }
+    let mut i = 0;
+    while i < raw_pixels_size {
+        let pixel = convert_nibbles_to_u32(&[raw_pixels[i], raw_pixels[i+1], raw_pixels[i+2]]);
+        *pixel_map.entry(pixel).or_insert(0) += 1;
+        i += 3;
     }
 
-    fn convert_nibbles_to_u32(values: &[u8]) -> u32{
-        let mut ret = 0;
-        for &i in values {
-            ret = ret << 8 | i as u32;
+    let mut sorted_pixels = Vec::from_iter(pixel_map);
+    sorted_pixels.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+
+
+    i = 0;
+    while i < num_colors {
+         match sorted_pixels.get(i) {
+            Some(&(pixel, count)) => {
+                println!("#{:X} - {} pixels", pixel, count);
+            },
+            None => break,
         }
-        ret
+        i += 1;
     }
+}
+
+fn convert_nibbles_to_u32(values: &[u8]) -> u32{
+    let mut ret = 0;
+    for &i in values {
+        ret = ret << 8 | i as u32;
+    }
+    ret
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::env;
 extern crate image;
 
 mod lib;
-use lib::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 3 || args.len() > 3 {
+        panic!("Not enough or too many arguments");
+    }
     let filename = &args[1];
+    let num_colors = args[2].parse::<usize>().expect("Expected an integer");
 
-    fetchColors(filename, 5);
+    lib::fetch_colors(filename, num_colors);
 }
